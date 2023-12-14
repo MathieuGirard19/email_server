d82d814a6402f4a3076e97825dc57960f8185fb3
mgirar3 <35713734+MathieuGirard19@users.noreply.github.com>
2023-12-14 17:25:05 -0500

    add cors

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config()
+
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.setHeader('Content-Type', 'text/html');
